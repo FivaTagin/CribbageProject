587c2dfc121fc461ea8289ad244ecdd1bd4ee00f
fiva1987 <fiva1987@gmail.com>
2018-09-17 21:47:47 +1000

    Revert "[HandValue] the structure of care created"
    This reverts commit 7a8efe2fb394e1da907a31f62ee083ea4d5988a9.

--- EvaluaeAHand/HandValue.java
+++ EvaluaeAHand/HandValue.java
@@ -6,19 +6,12 @@
 ;* You may not reproduce, distribute, publish, display, perform, modify, adapt,
 ;* transmit, broadcast, present, recite, release, license or otherwise exploit
 ;* any part of this publication in any form, by any means, without the prior
-;* written permission from Chugn-Chien,An [fiva1987@gmail.com].
+;* written permission of Insyde Software Corporation.
 ;*
 ;******************************************************************************
 */
 
-package CribbageProject.EvaluaeAHand;
-
-import CribbageProject.CommonLib.StructOfCards;
-// import CribbageProject.CommonLib.StructOfCards;
-
 public class HandValue {
-   
-  // private StructOfCards mCards = new StructOfCards ();
 
   public static void main(String[] args1) { 
 
