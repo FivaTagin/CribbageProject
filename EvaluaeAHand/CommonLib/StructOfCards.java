06877ed698c533cd4f1075d43c25fafd0bb037d4
fiva1987 <fiva1987@gmail.com>
2018-09-20 16:03:56 +1000

    [HandValue] the srtucture of cards
    new function : get cardface's values via class: structureOfCard

--- EvaluaeAHand/CommonLib/StructOfCards.java
+++ EvaluaeAHand/CommonLib/StructOfCards.java
@@ -12,9 +12,44 @@
 package CommonLib;
 
 import java.util.Random;
+import java.lang.*;
+import java.util.*;
+enum Rank { 
+  A(1) ,
+  DEUCE(2), 
+  THREE(3), 
+  FOUR(4), 
+  FIVE(5), 
+  SIX(6), 
+  SEVEN(7), 
+  EIGHT(8), 
+  NINE(9), 
+  T(10), 
+  J(11), 
+  Q(12), 
+  K(13);
+  
+  private int index;
+  private Rank (int Value) {
+    this.index = Value;
 
-enum Rank { DEUCE, THREE, FOUR, FIVE, SIX, SEVEN, EIGHT, NINE, T, J, Q, K, A }
-enum Suit { clubs, diamonds, hearts, spades }
+  }
+}
+enum Suit { C(0), D(1), H(2), S(3) ;
+  private int index = 0;
+
+  private Suit (int Value) {
+    this.index = Value;
+  }
+
+
+  public int getValue () {
+    return this.index;
+  }
+
+
+
+}
  public class StructOfCards {
 
     //
@@ -29,9 +64,29 @@ enum Suit { clubs, diamonds, hearts, spades }
     private static final int VAR_RANK_NUM = 13;
     private static final Boolean VAR_UNPICKED = true;
     private static final Boolean VAR_PICKED = false;
+    private static final int VAR_SUIT_CLUB = 0;
+    private static final int VAR_SUIT_DIAMOND = 1;
+    private static final int VAR_SUIT_HEART = 2;
+    private static final int VAR_SUIT_SPADE = 3;
+    private static final int VAR_RANK_ACE = 1;
+    private static final int VAR_RANK_DEUCE = 2;
+    private static final int VAR_RANK_THIRD = 3;
+    private static final int VAR_RANK_FOUR = 4;
+    private static final int VAR_RANK_FIVE = 5;
+    private static final int VAR_RANK_SIX = 6;
+    private static final int VAR_RANK_SEVEN = 7;
+    private static final int VAR_RANK_EIGHT = 8;
+    private static final int VAR_RANK_NIGH = 9;
+    private static final int VAR_RANK_TEN = 10;
+    private static final int VAR_RANK_JACK = 11;
+    private static final int VAR_RANK_QUEEN = 12;
+    private static final int VAR_RANK_KING = 13;
+
 
     public static final int VAR_CARDS_NUM = VAR_SUIT_NUM * VAR_RANK_NUM;
-    public static final int VAR_FAILURE_NUM = 666;
+    public static final int VAR_FAILURE_NUM = -666;
+    public static final int INDEX_RANK_OF_CARDS = 0;
+    public static final int INDEX_SUIT_OF_CARDS = 1;
     
     //
     // Define Global Values
@@ -52,11 +107,29 @@ enum Suit { clubs, diamonds, hearts, spades }
     //
     // Functions
     //
+
+
+/** 
+ *---------------------------------------------------------------------------------------
+ *
+ *  funcInitCards
+ *
+ *  Description:
+ *     Initialise a new pack of card
+ *
+ *  Parameters:
+ *    @param[in]     N/A
+ *  
+ *
+ *    @retval         PMODULE_STATUS
+ *
+ *---------------------------------------------------------------------------------------
+ **/
     private static void funcInitCards () {
       for ( Suit  suit : Suit.values() )
 		for (Rank rank: Rank.values()) {
             mCardsFlag [suit.ordinal()][rank.ordinal()] = VAR_UNPICKED;
-            mCards[suit.ordinal()][rank.ordinal()] = rank.name() + " of " + suit.name();
+            mCards[suit.ordinal()][rank.ordinal()] = rank.name() + suit.name();
         }
     }
 
@@ -132,19 +205,115 @@ enum Suit { clubs, diamonds, hearts, spades }
  /**
  *---------------------------------------------------------------------------------------
  *
- *  funcCheckPackofCard
+ *  funcReachCards
  *
  *  Description:
- *     Check this pack of card has been picked over or not.
+ *     Reach a card and transcript to a readable form.
  *
  *  Parameters:
- *    @param[in]     N/A
+ *    @param[in]     String  Card : a single card.
  *  
  *
- *    @retval        True 
- *    @retval        False 
+ *    @retval        Int     gRank : the Rank of the Card.
+ *    @retval        Int     gSuit : the Suit of the Card.
  *
  *---------------------------------------------------------------------------------------
  **/
+  public static void funcReachCards(String Card) {
+   
+
+    gRank = funcGetRank(Card.charAt(INDEX_RANK_OF_CARDS));
+    gSuit = funcGetSuit(Card.charAt(INDEX_SUIT_OF_CARDS));
+  }
+ /**
+ *---------------------------------------------------------------------------------------
+ *
+ *  funcGetSuit
+ *
+ *  Description:
+ *     Reach a card's Suit
+ *
+ *  Parameters:
+ *    @param[in]     Char  Suit : the suit of the card
+ *  
+ *
+ *    @retval        Int   VarSuit : the suit of the integer form
+ *
+ *---------------------------------------------------------------------------------------
+ **/
+  private static int funcGetSuit (char cSuit) {
+    int VarSuit = 0;
+    Suit eSuit;
+
+    switch (cSuit) {
+
+      case 'S':
+        VarSuit = VAR_SUIT_SPADE;
+      break;
+      case 'D':
+        VarSuit = VAR_SUIT_DIAMOND;
+      break;
+      case 'C':
+        VarSuit = VAR_SUIT_CLUB;
+      break;
+      case 'H':
+        VarSuit = VAR_SUIT_HEART;
+      break;
+      default:
+        VarSuit = VAR_FAILURE_NUM;  
+      break;
+    }
+   
+    return VarSuit;
+  }
+
+  /**
+ *---------------------------------------------------------------------------------------
+ *
+ *  funcGetSuit
+ *
+ *  Description:
+ *     Reach a card's Rank
+ *
+ *  Parameters:
+ *    @param[in]     Char  Rank : the Rank of the card
+ *  
+ *
+ *    @retval        Int   VarRank : the Rank of the integer form
+ *
+ *---------------------------------------------------------------------------------------
+ **/
+  private static int funcGetRank (char cRank) {
+    int VarRank = 0;
+    Rank rank;
+    //
+    // translate card from char form to integer form
+    //
+    switch (cRank) {
     
+      case 'A' :
+        VarRank = VAR_RANK_ACE;
+      break;
+      case 'T' :
+        VarRank = VAR_RANK_TEN;
+      break;
+      case 'J' :
+        VarRank = VAR_RANK_JACK;
+      break;
+      case 'Q' :
+        VarRank = VAR_RANK_QUEEN;
+      break;
+      case 'K' :
+        VarRank = VAR_RANK_KING;
+      break;
+      default :
+        VarRank = Integer.parseInt(String.valueOf(cRank));
+      break;
+    }
+    
+    if (VarRank < 1 || VarRank > 13) VarRank = VAR_FAILURE_NUM;
+
+    return VarRank;
+  }
+
 } 
--- EvaluaeAHand/HandValue.java
+++ EvaluaeAHand/HandValue.java
@@ -17,33 +17,54 @@ public class HandValue {
   //
   // Define Values
   //
-  
+  private static final int VAL_NUM_SINGLE_ROUND = 5;
 
   //
   // Define Global Class
   //
   private static StructOfCards mCards = new StructOfCards("PACK ONE");
   private static GameRule mGame = new GameRule();
-  private static int VAL_NUM_SINGLE_ROUND = 5;
+  
 
-  public static String [] mInputCards = new String [VAL_NUM_SINGLE_ROUND];
+  public static int [] mInFaceRank = new int [VAL_NUM_SINGLE_ROUND];
+  public static int [] mInFaceSuit = new int [VAL_NUM_SINGLE_ROUND];
+
+  public static CribbageRank CrbR;
   
 	public static void main (String [] Str) {
 
+    if (Str.length != VAL_NUM_SINGLE_ROUND) {
+      // error handle
+      System.out.println ("Error! ONLY Input 5 Cards due to the rule! ");
+      return;
+    }
+    
     funcReachCards(Str, Str.length);
+
+
     
 		return;
   }
   
   private static void funcReachCards (String [] Str, int Size) {
-    for (int i =0; i< Str.length; i++)
-      System.out.println("NAME : "+ Str[i]);
+
+    System.out.println(CrbR.KING);
+    
+    for (int i =0; i< Str.length; i++) {
+      System.out.print("NAME : "+ Str[i].charAt(mCards.INDEX_RANK_OF_CARDS) + Str[i].charAt(mCards.INDEX_SUIT_OF_CARDS));
+      mCards.funcReachCards (Str[i]);
+      mInFaceRank [i] = mCards.gRank;
+      mInFaceSuit [i] = mCards.gSuit;
+      System.out.println(" Value : " + mInFaceRank[i] + mInFaceSuit[i]);
+    }
+
+      
     return;
   }
 }
 
-public class ARoundOfCards {
+// public class oARoundOfCards {
 
 
-  
-}
+
+// }
